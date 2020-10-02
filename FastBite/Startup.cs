cfacc431a8111d757a787508b2cf7af8779c5272
pradeep-7161 <64249213+pradeep-7161@users.noreply.github.com>
2020-10-02 15:51:58 +0530

    Update Startup.cs

--- FastBite/Startup.cs
+++ FastBite/Startup.cs
@@ -1,22 +1,19 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodDelivery.Repository;
+using FoodDelivery.Service.Foods;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
-using FastBite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using FastBite.Services;
-using Microsoft.AspNetCore.Identity.UI.Services;
 
-namespace FastBite
+namespace FoodDelivery
 {
     public class Startup
     {
@@ -30,66 +27,54 @@ namespace FastBite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddDbContext<ApplicationDbContext>(options =>  
-            //     options.UseSqlServer(
-            //         Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<IdentityUser,IdentityRole>()
-            .AddDefaultTokenProviders()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
-               services.AddRazorPages()
-        .AddRazorRuntimeCompilation();
-            services.AddSingleton<IEmailSender,EmailSender>();
+            //call this in case you need aspnet-user-authtype/aspnet-user-identity
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseInMemoryDatabase("QuickBite"));
-            services.ConfigureApplicationCookie(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection"),
+                builder => builder.MigrationsAssembly("FoodDelivery")));
 
-{
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
-          options.LoginPath = $"/Identity/Account/Login";
+            services.AddTransient<IFoodService, FoodService>();
+            services.AddTransient<IFoodTypeService, FoodTypeService>();
 
-          options.LogoutPath = $"/Identity/Account/Logout";
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(ApplicationDbContext));
 
-          options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-
-});
-           //services.AddRazorPages();
-           services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-           services.AddSession(options =>{
-               options.Cookie.IsEssential=true;
-               options.IdleTimeout = TimeSpan.FromMinutes(15);
-               options.Cookie.HttpOnly= true;
-           });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-         //   app.UseHttpsRedirection();
+
             app.UseStaticFiles();
-            app.UseSession();
-            app.UseRouting();
+            app.UseCookiePolicy();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+            //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            //{
+            //    HotModuleReplacement = true
+            //});
 
-           app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
